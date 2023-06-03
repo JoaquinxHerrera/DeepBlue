96f47d481c99adf35b94f0d70bb89a8ef59dbe11
JoaquinxHerrera <joaquin.xherrera1@gmail.com>
2023-06-03 12:18:27 -0300

    cambios en proyecto

--- js/main.js
+++ js/main.js
@@ -1,59 +1,69 @@
 const productos = [
   {
+    id: 1,
     nombre: "Snorkel amarillo",
     precio: 30,
     stock: 10,
     urlImg:"https://i.ibb.co/2dD9Gvp/eccd697ca193a1661ad8a38dc1abfb104f8773fb2b824411f4f9521a7b7e01a210306.jpg"
   },
   {
+    id: 2,
     nombre: "Snorkel azul + Mascara",
     precio: 60,
     stock: 15,
     urlImg:"https://i.ibb.co/HP3JbJn/b281a81f7645ffca5dbf074cdbb46405deaa51313f4098dbf8e045a96bb5a72010306.jpg"
   },
   {
+    id: 3,
     nombre: "Snorkel rojo + Mascara",
     precio: 60,
     stock: 7,
     urlImg:"https://i.ibb.co/yPZ0wbw/e438486c01efd7c13bc4d4779e233ebeac8300e497ae45660a46165b187db33610306.png"
   },
   {
+    id: 4,
     nombre: "Neoprene azul",
     precio: 130,
     stock: 5,
     urlImg:"https://i.ibb.co/Bq9mk40/03e44f14b078f5a5f259f43f64afaa5a00df4774ca08559b785887b285a5b8ab10306.jpg"
   },
   {
+    id: 5,
     nombre: "Neoprene negro",
     precio: 6,
     stock: 120,
     urlImg:"https://i.ibb.co/8NYSVpf/a10c3935790cef0729ba3c485cf43d794b619d0de9bb70adfa65d5a33e52901910306.jpg"
   },
   {
+    id: 6,
     nombre: "Aletas negras",
     precio: 30,
     stock: 10,
     urlImg:"https://i.ibb.co/SNH5K3q/86d80556509ee1d5a01b9f5cea6efd51eea9f2631a7fb848219f2a5ab1b7f13a10306.jpg"
   },
   {
+    id: 7,
     nombre: "Aletas rojas",
     precio: 30,
     stock: 12,
     urlImg:"https://i.ibb.co/v19xj4D/86789d3faf0e2cd1c04f311e824eb83cdf49dc6510fc7982827341d8a778156c10306.webp"
   },
   {
+    id: 8,
     nombre: "Linterna",
     precio: 20,
     stock: 20,
     urlImg:"https://i.ibb.co/Zm6HfZr/c6fbe6e92a08291230aeadd1eca05e30b3de74fd82fee893f166ffb0065383ba10306.jpg"
   },
   {
+    id: 9,
     nombre: "Casco neoprene",
     precio: 30,
     stock: 10,
     urlImg:"https://i.ibb.co/R7TP5Xz/4b1898faecf8e33955d2b7bce69136ff8bcf1cc5cfc39ccf0d0f7bc111e7e3ab10306.webp"
   },
   {
+    id: 10,
     nombre: "Guante neoprene",
     precio: 30,
     stock: 10,
@@ -62,18 +72,24 @@ const productos = [
 
 ]
 
-const verProducto = ({nombre,precio,stock,urlImg}) => {
+const carrito = JSON.parse(localStorage.getItem("carrito")) ?? []
+const verProducto = ({id,nombre,precio,stock,urlImg}) => {
   const contenedorTarjetas = document.querySelector("#contenedorTarjetas")
   const tarjeta = document.createElement("div")
   tarjeta.className = "tarjeta"
   tarjeta.innerHTML = `
                       <img src="${urlImg}" alt="">
                       <div class="contenido">
-                      <h3> <b>${nombre}</b></h3>
-                      <span><b>Precio:</b> $${precio} <b>Stock:</b>${stock}</span>
+                        <h3> <b>${nombre}</b></h3>
+                        <span><b>Precio:</b> ${precio} <b>Stock:</b> ${stock}</span>
                       </div>
-                      <form id="formCarrito">
-                      <input type="number" value="1" min="1" max="${stock}" >
+                      <form id="formCarrito${id}">
+                        <div>
+                          <input name="id" type="hidden"  value="${id}">
+                        </div>
+                        <div class "inputCantidad">
+                          <input class="cantidadMargen \" name="cantidad" type="number" value="1" min="1" max="${stock}" >
+                        </div>
                       <button type="submit">Add to cart</button>   
                       </form>
                                        
@@ -81,10 +97,16 @@ const verProducto = ({nombre,precio,stock,urlImg}) => {
   contenedorTarjetas.append(tarjeta)
 }
 
-const agregarCarrito = () =>{
-  const formCarrito = document.querySelector("#formCarrito")
+const agregarCarrito = (id) =>{
+  const formCarrito = document.querySelector("#formCarrito"+ id)
   formCarrito.addEventListener("submit",(e)=>{
     e.preventDefault()
+    const cantidad = e.target.children["cantidad"].value;
+    carrito.push({
+      id,
+      cantidad
+    })
+    localStorage.setItem("carrito",JSON.stringify(carrito))
   })
 }
 
@@ -93,13 +115,14 @@ const verProductos = () =>{
   productos.forEach(producto =>{
     if(producto.stock !=0){
       verProducto(producto)
+      agregarCarrito(producto.id)
     }
     
   })
 }
 
 verProductos()
-
+/*
 //Paquetes
 class Paquete{
   constructor(letra,nombre,precio){
@@ -122,7 +145,7 @@ const paquetesA = carrito.filter((paquete)=>paquete.letra==="A");
 const paquetesB = carrito.filter((paquete)=>paquete.letra==="B");
 const paquetesC = carrito.filter((paquete)=>paquete.letra==="C");
 */
-
+/*
 let paquetesA = 0
 let paquetesB = 0
 let paquetesC = 0
@@ -205,4 +228,4 @@ if(seleccion != "NO"){
   
   alert(`Carrito:\n ===================================== \n > Paquete A - Snorkeling: ${paquetesA} paquete/s x 60$ c/u. \n > Paquete B - Shipwreck: ${paquetesB} paquete/s x 100$ c/u. \n > Paquete C - Dive with sharks: ${paquetesC} paquete/s x 120$ c/u.\n===================================== \n \n Subtotal: ${paquetesA*60 + paquetesB*100+paquetesC*120}`)
   alert("Gracias por su compra")  
-}
+}*/
